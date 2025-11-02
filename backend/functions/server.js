015b89c66c2e805ddf35faae0e651acead51dc23
Shubham Rajesh Saroj <57361979+shubhamrsaroj@users.noreply.github.com>
2025-11-03 00:49:57 +0530

    new files

--- backend/functions/server.js
+++
@@ -1,9 +0,0 @@
-import serverless from 'serverless-http';
-import express from 'express';
-import app from '../server.js';
-
-export const handler = async (event, context) => {
-  const serverlessHandler = serverless(app);
-  const result = await serverlessHandler(event, context);
-  return result;
-};
